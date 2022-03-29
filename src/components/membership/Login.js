1225b789e2c3764acdd4de819d61c72eed9cbedd
icallitnewart <icallitnewart@gmail.com>
2022-03-30 01:49:31 +0900

    Feat: Login 컴포넌트 추가

--- src/components/membership/Login.js
+++ src/components/membership/Login.js
@@ -1,7 +1,100 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Login() {
+    const initValue = { 
+        userid: "",
+        password: ""
+    }
+    const [ inputs, setInputs ] = useState(initValue);
+    const handleChange = (e)=> {
+        const { name, value } = e.target;
+        setInputs({ ...inputs, [name] : value });
+    };
+    const handleCheck = (e)=> {
+        const { name } = e.target;
+        const isChecked = e.target.checked;
+        setInputs({ ...inputs, [name] : isChecked });
+    }
+
+    console.log(inputs);
+
     return (
         <section className="login">
-            login
+            <div className="inner">
+                <form action="/" method="post" className="loginForm">
+                    <h1>
+                        <span>UNSPACE</span>
+                    </h1>
+                    <fieldset>
+                        <legend className="hidden">
+                            Login Form
+                        </legend>
+                        <table summary="User's id, password for login">
+                            <caption className="hidden">Login Form</caption>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        <label htmlFor="userid">USER ID</label>  
+                                    </th>
+                                    <td>
+                                        <input 
+                                            type="text"
+                                            name="userid" 
+                                            id="userid"
+                                            onChange={handleChange}
+                                            value={inputs.userid}
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <label htmlFor="password">
+                                        PASSWORD
+                                        </label>
+                                    </th>
+                                    <td>
+                                        <input 
+                                            type="password" 
+                                            name="password" 
+                                            id="password"
+                                            onChange={handleChange}
+                                            value={inputs.password}
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="remember">
+                                            <input 
+                                                type="checkbox" 
+                                                name="remember" 
+                                                id="remember" 
+                                                onChange={handleCheck}
+                                            /> 
+                                            remember me
+                                        </label>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <input type="submit" value="LOGIN" />
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                    <p><span>Forgot your ID or password?</span></p>
+                </form>
+                <div className="banner">
+                    <img src="img/login.jpg" alt="sign up photo" />
+                    <div className="textBox">
+                        <h2>Don't have an account yet?</h2>
+                        <h3>Come Join Us!</h3>
+                        <Link to="/membership/signup">SIGN UP</Link>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
