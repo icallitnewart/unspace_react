b145f35fece9071c6b70330aa0bbaa0df1e5a48a
icallitnewart <icallitnewart@gmail.com>
2022-04-08 13:06:48 +0900

    Feat: Header 모바일 메뉴 버튼 클릭시 토글 사이드바 생성 기능 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Header({ type }) {
+    const [ mobileMenu, setMobileMenu ] = useState(false);
+    const [ slideEffect, setSlideEffect ] = useState(false);
     const active = {
         color: "salmon",
-        fontWeight: "bold",
-    }
+        fontWeight: 600
+    };
+    const activeMobile = {
+        color: "salmon"
+    };
+
+    useEffect(()=> {
+        (mobileMenu) && setSlideEffect(true)
+    }, [mobileMenu]);
+
+    useEffect(()=> {
+        if(!slideEffect) {
+            const timer = setTimeout(()=> setMobileMenu(false), 1000);
+            return ()=> clearTimeout(timer);
+        }
+    }, [slideEffect]);
 
     return (
         <header 
-            id={type === "sub" ? "header_sub" : "header"}
+            id={"header"}
+            className={type === "sub" ? "header_sub" : ""}
         >
             <div className="inner">
                 <h1><NavLink exact to="/">UNSPACE</NavLink></h1>
@@ -19,38 +37,52 @@ function Header({ type }) {
                         <li><NavLink activeStyle={active} exact to="/media/projects">MEDIA</NavLink></li>
                         <li><NavLink activeStyle={active} exact to="/community/faq">COMMUNITY</NavLink></li>
                         <li><NavLink activeStyle={active} exact to="/contact">CONTACT</NavLink></li>
-                        <li><NavLink activeStyle={active} exact to="/membership/login"><i className="fas fa-user-circle"></i></NavLink></li>
+                        <li><NavLink activeStyle={active} exact to="/membership/login">MY PAGE</NavLink></li>
                     </ul>
                 </nav>
         
-                <a href="#" className="btnCall">
+                <button className="btnCall" onClick={(()=> setMobileMenu(true))}>
                     <span>메뉴호출</span>
                     <i className="fas fa-bars"></i>
-                </a>
+                </button>
             </div>
                 
-            <nav className="menuMobile">
+            {mobileMenu && 
+            <nav className={slideEffect ? "menuMobile on" : "menuMobile"}>
                 <h1>
-                    <a href="index.html" className="logo">
-                        <span>UNSPACE</span>
-                        <span>Design your space</span>
-                    </a>
+                    <NavLink activeStyle={active} className="logo" exact to="/">
+                        UNSPACE
+                    </NavLink>
+                    <span>Design your space</span>
                 </h1>
                 <h2>
-                    <a href="#">Login</a> / 
-                    <a href="join.html">Sign Up</a>
+                    <NavLink exact to="/membership/login">Login</NavLink> | 
+                    <NavLink exact to="/membership/signup">Sign Up</NavLink>
                 </h2>
                 
                 <ul id="gnbMobile">
-                    <li className="on"><a href="index.html">HOME</a></li>
-                    <li><a href="about.html">ABOUT US</a></li>
-                    <li><a href="projects.html">PROJECTS</a></li>
-                    <li><a href="media.html">MEDIA</a></li>
-                    <li><a href="contact.html">CONTACT</a></li>
+                    <li>
+                        <NavLink activeStyle={activeMobile} exact to="/">HOME</NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={activeMobile} exact to="/about">ABOUT US</NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={activeMobile} exact to="/media/projects">MEDIA</NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={activeMobile} exact to="/community/faq">COMMUNITY</NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeStyle={activeMobile} exact to="/contact">CONTACT</NavLink>
+                    </li>
                 </ul>
-        
-                <a href="#" className="closeBtn"><span className="hidden">닫기</span></a>
+
+                <button className="closeBtn" onClick={()=> setSlideEffect(false)}>
+                    <span className="hidden">닫기</span>
+                </button>
             </nav>
+            }
         </header>
     )
 }
