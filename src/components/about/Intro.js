eb9e98b29820bea80000362368579d18de5f066d
icallitnewart <icallitnewart@gmail.com>
2022-04-11 01:32:45 +0900

    Design: About 페이지 반응형 CSS 적용

--- src/components/about/Intro.js
+++ src/components/about/Intro.js
@@ -4,7 +4,7 @@ function Intro(props, ref) {
     return (
         <section className="intro" ref={ref}>
             <div className="inner">
-                <div className="wrap">
+                <div className="pic">
                     <article style={{ 
                     background: "url(../img/about_intro1.jpg) center/cover" }}>
                         <div className="textBox">
@@ -19,11 +19,11 @@ function Intro(props, ref) {
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, ipsa incidunt. Quam nihil distinctio vel!</p>
                         </div>
                     </article>
-                    <article>
-                        <h1>ART OF SPACING</h1>
-                        <span></span>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis iusto fugiat excepturi debitis laboriosam obcaecati optio magni, aspernatur iure assumenda facilis libero numquam quae. Exercitationem velit assumenda quia quis iusto.</p>
-                    </article>
+                </div>
+                <div className="textBox">
+                    <h1>ART OF SPACING</h1>
+                    <span></span>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis iusto fugiat excepturi debitis laboriosam obcaecati optio magni, aspernatur iure assumenda facilis libero numquam quae. Exercitationem velit assumenda quia quis iusto.</p>
                 </div>
             </div>
         </section>
