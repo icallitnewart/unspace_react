347cc1ed22b317e4174b9495a18a8ebb68cbcf34
icallitnewart <icallitnewart@gmail.com>
2022-04-22 01:54:04 +0900

    Fix: 첫번째 외의 페이지 번호에서 검색시 검색 결과 안 뜨는 문제 해결

--- src/components/community/Notice.js
+++ src/components/community/Notice.js
@@ -83,6 +83,7 @@ function Notice() {
                                 data={data} 
                                 setItems={setItems} 
                                 setHighlight={setHighlight}
+                                resetPage={setPage}
                             />
                             <Pagination
                                 totalItems={totalItems}
